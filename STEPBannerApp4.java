6547f5421e0fd5a014526e2cdd85502534f06364
vaishu-ani <vaishnavisriram1807@gmail.com>
2026-02-16 21:02:52 +0530

    Add files via upload

---
+++ STEPBannerApp4.java
@@ -0,0 +1,12 @@
+ public class STEPBannerApp4 {
+      public static void main(String[] args){
+	        System.out.println("...");		   
+	        System.out.println("   ***   " + "   " + "   ***   " + "   " + " ******  " + "   " + "  *****  ");
+			System.out.println(" **   ** " + "   " + " **   ** " + "   " + " **    **" + "   " + " **   ** ");
+			System.out.println("**     **" + "   " + "**     **" + "   " + " **    **" + "   " + " **      ");
+			System.out.println("**     **" + "   " + "**     **" + "   " + " ******  " + "   " + "  *****  ");
+			System.out.println("**     **" + "   " + "**     **" + "   " + " **      " + "   " + "      ** ");
+			System.out.println(" **   ** " + "   " + " **   ** " + "   " + " **      " + "   " + " **   ** ");
+			System.out.println("   ***   " + "   " + "   ***   " + "   " + " **      " + "   " + "  *****  ");
+    }
+}
