eaac99bf42bc053cef2ddb34569ce9cb17bae4c6
vaishu-ani <vaishnavisriram1807@gmail.com>
2026-02-16 22:07:42 +0530

    Add files via upload

---
+++ STEPBannerApp3.java
@@ -0,0 +1,15 @@
+
+        public class STEPBannerApp3 {
+      public static void main(String[] args){
+	
+		    System.out.println("...");		   
+	        System.out.println(String.join(" ","   ***   " , " ", "   ***   " , " ", " ******  " , " ", "  *****  "));
+			System.out.println(String.join(" "," **   ** " , " ", " **   ** " , " ", " **    **" , " ", " **   ** "));
+			System.out.println(String.join(" ","**     **" , " ", "**     **" , " ", " **    **" , " ", " **      "));
+			System.out.println(String.join(" ","**     **" , " ", "**     **" , " ", " ******  " , " ", "  *****  "));
+			System.out.println(String.join(" ","**     **" , " ", "**     **" , " ", " **      " , " ", "      ** "));
+			System.out.println(String.join(" "," **   ** " , " ", " **   ** " , " ", " **      " , " ", " **   ** "));
+			System.out.println(String.join(" ","   ***   " , " ", "   ***   " , " ", " **      " , " ", "  *****  "));
+    }
+}
+
